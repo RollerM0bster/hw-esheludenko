583b61e388fc8733e2572716ad8ac52f2819606e
esheludenko <esheludenko@diasoft.ru>
2024-09-14 18:52:22 +0300

    HW2 is completed

--- hw02_unpack_string/unpack.go
+++ hw02_unpack_string/unpack.go
@@ -2,11 +2,54 @@ package hw02unpackstring
 
 import (
 	"errors"
+	"strconv"
+	"strings"
+	"unicode"
 )
 
 var ErrInvalidString = errors.New("invalid string")
 
-func Unpack(_ string) (string, error) {
-	// Place your code here.
-	return "", nil
+func Unpack(str string) (string, error) {
+	_, err := strconv.Atoi(str)
+	runes := []rune(str)
+	if err != nil {
+		builder := strings.Builder{}
+		for i := 0; i < len(runes)-1; i++ {
+			processErr := processSymbol(i, runes[i], runes[i+1], &builder, len(runes)-1)
+			if processErr != nil {
+				return "", processErr
+			}
+		}
+		return builder.String(), nil
+	}
+	return "", ErrInvalidString
+}
+
+func processSymbol(index int, r1 rune, next rune, builder *strings.Builder, len int) error {
+	if index == 0 {
+		if isDigit(r1) == true {
+			return ErrInvalidString
+		}
+	}
+	if unicode.IsLetter(r1) {
+		if unicode.IsDigit(next) {
+			digit, _ := strconv.Atoi(string(next))
+			builder.WriteString(strings.Repeat(string(r1), digit))
+		} else {
+			builder.WriteString(string(r1))
+		}
+
+	} else {
+		if isDigit(next) {
+			return ErrInvalidString
+		}
+	}
+	if index == len-1 && unicode.IsLetter(next) {
+		builder.WriteString(string(next))
+	}
+	return nil
+}
+
+func isDigit(r rune) bool {
+	return r >= '0' && r <= '9'
 }
