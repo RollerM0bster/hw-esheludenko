49d47855eb56cd1cb0a7d1d145865c7d7b3960aa
esheludenko <esheludenko@diasoft.ru>
2024-11-23 17:08:21 +0300

    HW6 pt1

--- hw06_pipeline_execution/pipeline.go
+++ hw06_pipeline_execution/pipeline.go
@@ -9,6 +9,31 @@ type (
 type Stage func(in In) (out Out)
 
 func ExecutePipeline(in In, done In, stages ...Stage) Out {
-	// Place your code here.
-	return nil
+	for _, stage := range stages {
+		in = moveToNext(stage(in), done)
+	}
+	return in
+}
+
+func moveToNext(in In, done In) Out {
+	out := make(Bi)
+	go func() {
+		defer close(out) // Ensure the output channel is always closed
+		for {
+			select {
+			case v, ok := <-in:
+				if !ok {
+					return // Input channel closed, stop processing
+				}
+				select {
+				case out <- v: // Pass value downstream
+				case <-done: // Stop processing if done signal is received
+					return
+				}
+			case <-done: // Stop processing if done signal is received
+				return
+			}
+		}
+	}()
+	return out
 }
