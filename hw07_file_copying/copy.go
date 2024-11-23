e82c48ec49cb0748db3d176a135079b05a8798bd
esheludenko <esheludenko@diasoft.ru>
2024-11-23 20:47:17 +0300

    HW7 - done

--- hw07_file_copying/copy.go
+++ hw07_file_copying/copy.go
@@ -2,14 +2,58 @@ package main
 
 import (
 	"errors"
+	"io"
+	"os"
+
+	"github.com/cheggaaa/pb/v3"
 )
 
 var (
 	ErrUnsupportedFile       = errors.New("unsupported file")
 	ErrOffsetExceedsFileSize = errors.New("offset exceeds file size")
+	ErrFileNotExists         = errors.New("file does not exist")
 )
 
 func Copy(fromPath, toPath string, offset, limit int64) error {
-	// Place your code here.
+	var file *os.File
+	file, err := os.Open(fromPath)
+	if err != nil {
+		if os.IsNotExist(err) {
+			return ErrFileNotExists
+		}
+		return ErrUnsupportedFile
+	}
+	fileInfo, err := file.Stat()
+	if err != nil {
+		return ErrUnsupportedFile
+	}
+	fileSize := fileInfo.Size()
+	if offset > fileSize {
+		return ErrOffsetExceedsFileSize
+	}
+	if limit == 0 || limit > fileSize {
+		limit = fileSize
+	}
+	_, err = file.Seek(offset, io.SeekStart)
+	if err != nil {
+		return err
+	}
+	writeTo, err := os.Create(toPath)
+	if err != nil {
+		return err
+	}
+	defer writeTo.Close()
+
+	progressBar := pb.New(int(limit)).Start()
+	defer progressBar.Finish()
+
+	progressReader := progressBar.NewProxyReader(file)
+
+	_, err = io.CopyN(writeTo, progressReader, limit)
+	if err != nil && !errors.Is(err, io.EOF) {
+		return err
+	}
+
+	defer file.Close()
 	return nil
 }
--- hw07_file_copying/copy_test.go
+++ hw07_file_copying/copy_test.go
@@ -1,7 +1,92 @@
 package main
 
-import "testing"
+import (
+	"errors"
+	"os"
+	"testing"
+)
 
 func TestCopy(t *testing.T) {
-	// Place your code here.
+	srcFile, err := os.CreateTemp("", "srcFile*.txt")
+	if err != nil {
+		t.Fatalf("Failed to create temporary source file: %v", err)
+	}
+	defer os.Remove(srcFile.Name())
+	srcContent := []byte("Hello, World!")
+	srcFile.Write(srcContent)
+	srcFile.Close()
+
+	tests := []struct {
+		name        string
+		fromPath    string
+		toPath      string
+		offset      int64
+		limit       int64
+		wantErr     error
+		wantContent string
+	}{
+		{
+			name:        "Normal Copy",
+			fromPath:    srcFile.Name(),
+			toPath:      "dst_normal.txt",
+			offset:      0,
+			limit:       5,
+			wantErr:     nil,
+			wantContent: "Hello",
+		},
+		{
+			name:     "Offset Exceeds File Size",
+			fromPath: srcFile.Name(),
+			toPath:   "dst_offset_exceeds.txt",
+			offset:   int64(len(srcContent) + 1),
+			limit:    5,
+			wantErr:  ErrOffsetExceedsFileSize,
+		},
+		{
+			name:        "Limit Larger Than Remaining Data",
+			fromPath:    srcFile.Name(),
+			toPath:      "dst_limit_large.txt",
+			offset:      7,
+			limit:       50,
+			wantErr:     nil,
+			wantContent: "World!",
+		},
+		{
+			name:        "Zero Limit (Copy All from Offset)",
+			fromPath:    srcFile.Name(),
+			toPath:      "dst_zero_limit.txt",
+			offset:      7,
+			limit:       0,
+			wantErr:     nil,
+			wantContent: "World!",
+		},
+		{
+			name:     "Source File Does Not Exist",
+			fromPath: "nonexistent.txt",
+			toPath:   "dst_no_src.txt",
+			offset:   0,
+			limit:    5,
+			wantErr:  ErrFileNotExists,
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			err := Copy(tt.fromPath, tt.toPath, tt.offset, tt.limit)
+			if !errors.Is(err, tt.wantErr) {
+				t.Errorf("Copy() error = %v, wantErr = %v", err, tt.wantErr)
+				return
+			}
+			if tt.wantErr == nil {
+				content, err := os.ReadFile(tt.toPath)
+				if err != nil {
+					t.Fatalf("Failed to read destination file: %v", err)
+				}
+				if string(content) != tt.wantContent {
+					t.Errorf("File content = %q, wantContent = %q", string(content), tt.wantContent)
+				}
+				os.Remove(tt.toPath)
+			}
+		})
+	}
 }
