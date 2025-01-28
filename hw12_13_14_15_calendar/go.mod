32400c3338ee7fbd3d6e18a74e9b9c6f80ebdebf
esheludenko <esheludenko@diasoft.ru>
2025-01-28 23:49:51 +0300

    HW13 - done

--- hw12_13_14_15_calendar/go.mod
+++ hw12_13_14_15_calendar/go.mod
@@ -13,11 +13,13 @@ require (
 	github.com/jessevdk/go-flags v1.6.1
 	github.com/lib/pq v1.10.9
 	github.com/spf13/viper v1.19.0
+	github.com/stretchr/testify v1.9.0
 	golang.org/x/net v0.23.0
 )
 
 require (
 	github.com/asaskevich/govalidator v0.0.0-20230301143203-a9d515a09cc2 // indirect
+	github.com/davecgh/go-spew v1.1.2-0.20180830191138-d8f796af33cc // indirect
 	github.com/docker/go-units v0.5.0 // indirect
 	github.com/fsnotify/fsnotify v1.7.0 // indirect
 	github.com/go-openapi/analysis v0.23.0 // indirect
@@ -31,12 +33,14 @@ require (
 	github.com/mitchellh/mapstructure v1.5.0 // indirect
 	github.com/oklog/ulid v1.3.1 // indirect
 	github.com/pelletier/go-toml/v2 v2.2.2 // indirect
+	github.com/pmezard/go-difflib v1.0.1-0.20181226105442-5d4384ee4fb2 // indirect
 	github.com/sagikazarmark/locafero v0.4.0 // indirect
 	github.com/sagikazarmark/slog-shim v0.1.0 // indirect
 	github.com/sourcegraph/conc v0.3.0 // indirect
 	github.com/spf13/afero v1.11.0 // indirect
 	github.com/spf13/cast v1.6.0 // indirect
 	github.com/spf13/pflag v1.0.5 // indirect
+	github.com/stretchr/objx v0.5.2 // indirect
 	github.com/subosito/gotenv v1.6.0 // indirect
 	go.mongodb.org/mongo-driver v1.14.0 // indirect
 	go.uber.org/atomic v1.9.0 // indirect
--- hw12_13_14_15_calendar/internal/server/http/server_test.go
+++ hw12_13_14_15_calendar/internal/server/http/server_test.go
@@ -2,6 +2,10 @@ package internalhttp
 
 import (
 	"github.com/RollerM0bster/hw-esheludenko/hw12_13_14_15_calendar/models"
+	"github.com/RollerM0bster/hw-esheludenko/hw12_13_14_15_calendar/restapi/operations"
+	"github.com/go-openapi/runtime/middleware"
+	"github.com/go-openapi/strfmt"
+	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/mock"
 	"testing"
 	"time"
@@ -16,24 +20,68 @@ func (m *MockApp) FindEventsByWeek(weekStart time.Time) ([]*models.Event, error)
 	return args.Get(0).([]*models.Event), args.Error(1)
 }
 
-func TestGetEventsByWeekHandler(t *testing.T) {
-	//mockApp := new(MockApp)
-	//mockApp.On("FindEventsByWeek", mock.AnythingOfType("time.Time")).Return([]*models.Event{
-	//	{ID: 1, Title: "Event 1"},
-	//	{ID: 2, Title: "Event 2"},
-	//}, nil)
-	//server := &Server{app: mockApp}
-	//req, err := http.NewRequest("GET", "/events-by-week?weekStart=2024-10-25", nil)
-	//rec := httptest.NewRecorder()
-	//
-	//handler := http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-	//	params := operations.GetEventsByWeekParams{
-	//		WeekStart: strfmt.Date(time.Date(2024-10-25, 0, 0, 0, 0, 0, 0, time.UTC)),
-	//	}
-	//	res := server.GetEventsByWeekHandler(params)
-	//	if responder, ok := res.(middleware.ResponderFunc); ok {
-	//		responder(w, r)
-	//	}
-	//})
-	//handler.ServeHTTP(rec, req)
+func (m *MockApp) CreateEvent(event models.NewEvent) (int64, error) {
+	args := m.Called(event)
+	return args.Get(0).(int64), args.Error(1)
+}
+
+func (m *MockApp) DeleteEvent(id int64) error {
+	args := m.Called(id)
+	return args.Error(0)
+}
+
+func (m *MockApp) ChangeEvent(id int64, event models.NewEvent) error {
+	args := m.Called(id, event)
+	return args.Error(0)
+}
+
+func (m *MockApp) FindEventsByDay(day time.Time) ([]*models.Event, error) {
+	args := m.Called(day)
+	return args.Get(0).([]*models.Event), args.Error(1)
+}
+
+func (m *MockApp) FindEventsByMonth(day time.Time) ([]*models.Event, error) {
+	args := m.Called(day)
+	return args.Get(0).([]*models.Event), args.Error(1)
+}
+
+func TestGetEventsByWeekHandler_Unit(t *testing.T) {
+	mockApp := new(MockApp)
+
+	mockApp.On("FindEventsByWeek", time.Date(2024, 1, 27, 0, 0, 0, 0, time.UTC)).Return(
+		[]*models.Event{
+			{ID: 1, Title: "Event 1"},
+			{ID: 2, Title: "Event 2"},
+		}, nil,
+	)
+
+	handler := operations.GetEventsByWeekHandlerFunc(
+		func(params operations.GetEventsByWeekParams) middleware.Responder {
+			weekStart, err := time.Parse("2006-01-02", params.WeekStart.String())
+			if err != nil {
+				return &operations.GetEventsByWeekInternalServerError{Payload: &models.Error{Message: "Invalid date"}}
+			}
+
+			events, err := mockApp.FindEventsByWeek(weekStart)
+			if err != nil {
+				return &operations.GetEventsByWeekInternalServerError{Payload: &models.Error{Message: err.Error()}}
+			}
+
+			return &operations.GetEventsByWeekOK{Payload: events}
+		},
+	)
+
+	params := operations.GetEventsByWeekParams{
+		WeekStart: strfmt.Date(time.Date(2024, 1, 27, 0, 0, 0, 0, time.UTC)),
+	}
+
+	res := handler.Handle(params)
+
+	okRes, ok := res.(*operations.GetEventsByWeekOK)
+	assert.True(t, ok)
+	assert.Equal(t, 2, len(okRes.Payload))
+	assert.Equal(t, "Event 1", okRes.Payload[0].Title)
+
+	// Verify the mock call
+	mockApp.AssertCalled(t, "FindEventsByWeek", time.Date(2024, 1, 27, 0, 0, 0, 0, time.UTC))
 }
